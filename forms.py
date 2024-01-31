a638c7321759f4402d66b5d395668b2baf289425
IDGS-803-21000011 <paulovaladez813@gmail.com>
2024-01-30 18:07:16 -0600

    PEVG 30/01/2024 WTF forms

---
+++ forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form
+from wtforms import StringField, SelectField, RadioField, EmailField
+
+class UserForm(Form):
+    nombre = StringField('nombre')
+    apaterno = StringField('apaterno')
+    amaterno = StringField('amaterno')
+    edad = StringField('edad')
+    correo = StringField('correo')
